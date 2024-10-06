5e4866e48bf80c766c20ceee215b5c44eb62b6c2
bimalbenoy <bimalbenoy0@gmail.com>
2024-10-06 23:51:40 +0530

    Update messd.php

--- includes/messd.php
+++ includes/messd.php
@@ -5,27 +5,56 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require 'config.inc.php';
     session_start();
     
+   
+   
     
-    // Access the selected options from the POST data
-    //hello
     $lunchOption1 = $_POST['lunchOption1'];
     $lunchOption2 = $_POST['lunchOption2'];
+    $breakfast='';
+    $lunch='';
+
     $id=$_SESSION['roll'] ;
     global $conn;
+    $select="SELECT * from mess WHERE `mess`.`Student_id` = '$id'";
+    $idselect=mysqli_query($conn,$select);
+    $num=mysqli_num_rows($idselect);
+    if($num>0){
+    $delete=mysqli_query($conn,"DELETE FROM `mess`");
+    }
 
-    // Process the selected options (e.g., store in a database, perform calculations)
+   
     
     echo "Lunch Option 1: " . $lunchOption1;
     echo "<br>";
     echo "Lunch Option 2: " . $lunchOption2;
-    if (isset($lunchOption1)&&isset($lunchOption2)) {
-    $sql="INSERT INTO mess(orders,time_of_day,Student_id)
-    VALUES ('$lunchOption1,$lunchOption2','breakfast,lunch','$id' )";
-    }
+    if (isset($lunchOption1)){
+        $breakfast='breakfast';
+        $sql1="INSERT INTO mess(orders,time_of_day,Student_id)
+    VALUES ('$lunchOption1,$lunchOption2','$breakfast','$id' )";
+    $Execute1=mysqli_query($conn, $sql1);
+
+    } 
     
-    $Execute=mysqli_query($conn, $sql);
-    if($Execute){
-        echo 'Successfully completed';
+    if (isset($lunchOption2)){ 
+        $lunch='lunch';
+        $sql2="INSERT INTO mess(orders,time_of_day,Student_id)
+    VALUES ('$lunchOption1,$lunchOption2','$lunch','$id' )";
+    $Execute2=mysqli_query($conn, $sql2);
+    }
+
+   
+   
+    
+    if($delete){
+        echo 'deleted';
+    }
+
+    if($Execute1){
+        echo 'Successfully completed lunch';
+    }
+    elseif($Execute2){
+        echo 'Successfully completed breakfast';
+
     }
     else{
         echo 'failed';
