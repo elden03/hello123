fb1a93980a1e8437c2d5b736ab67eba980cd3657
elden03 <136626312+elden03@users.noreply.github.com>
2024-10-09 13:07:30 +0530

    Create feedj.php

---
+++ feedj.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+ <style>
+
+ </style>
+<link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+    
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Slide Navbar</title>
+        <link rel="stylesheet" type="text/css" href="slide navbar style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="main">  	
+            <input type="checkbox" id="chk" aria-hidden="true">
+    
+                <div class="signup">
+                    <form>
+                        <label for="chk" aria-hidden="true">Studt pay info</label>
+                        <input type="text" name="txt" placeholder="Student Name">
+                        <input type="text" name="email" placeholder="Student ID" >
+                        <input type="number" name="broj" placeholder="Fees to be paid">
+                        <button type="submit">submit</button>
+                        <input type="text" name="bimal" placeholder="student ID to Remove" >
+                       <button type="text">Remove Student</button>
+                        
+                    </form>
+                </div>
+    
+                
+    </body>
+    </html>
+
+</body>
+</html>
