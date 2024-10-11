55cb08cdf91812f65cacf27a913863be4d215bd9
Dhananjaynathkj <dhananjaynathkjkj@gmail.com>
2024-10-11 22:14:49 +0530

    Create djtable.php

---
+++ djtable.php
@@ -0,0 +1,91 @@
+<link rel="stylesheet" href="./style.css">
+</head>
+</head>
+<body>
+    
+    <table class="content-table">
+      <thead>
+        <tr>
+            <th>Student ID</th>
+            <th>Order</th>
+            <th>Time</th>
+            
+        </tr>
+    </thead>
+      <tbody>
+        <tr>
+            <td>1</td>
+            <td>Idiyappam</td>
+            <td>Breakfast</td>
+        </tr>
+    
+            <td>2</td>
+            <td>Puttu</td>
+            <td>Breakfast</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>noodles</td>
+            <td>Breakfast</td>
+        </tr>
+        <tr>
+            
+            <td>4</td>
+            <td>Dosa</td>
+            <td>Breadfast</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Uppuma</td>
+            <td>Breakfast</td>
+        </tr>
+        <tr>
+            
+            <td>6</td>
+            <td>Poratta</td>
+            <td>Dinner</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Appam</td>
+            <td>Breakfast</td>
+        </tr>
+        <tr>
+            
+            <td>8</td>
+            <td>Idily</td>
+            <td>Breakfast</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>chappathi</td>
+            <td>Dinner</td>
+        </tr>
+        <tr>
+           
+            <td>10</td>
+            <td>Biryani</td>
+            <td>Lunch</td>
+        </tr>
+        
+
+
+
+      
+    
+    </tbody>
+
+ </table>
+
+
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
