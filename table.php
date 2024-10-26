5162c0d7506ae67cda2af6c2bda7daa972587859
elden03 <136626312+elden03@users.noreply.github.com>
2024-10-26 10:03:35 +0530

    Update table.php

--- table.php
+++ table.php
@@ -4,8 +4,101 @@
 
 <link rel="stylesheet" href="./styletable.css">
 </head>
+<title> Allocated Rooms</title>
+	
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="keywords" content="Intrend Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!--bootsrap -->
+
+	<!--// Meta tag Keywords -->
+		
+	<!-- css files -->
+	<link rel="stylesheet" href="web_home/css_home/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="web_home/css_home/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+	<link rel="stylesheet" href="web_home/css_home/fontawesome-all.css"> <!-- Font-Awesome-Icons-CSS -->
+	<!-- //css files -->
+	
+	<!-- web-fonts -->
+	<link href="//fonts.googleapis.com/css?family=Poiret+One&amp;subset=cyrillic,latin-ext" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+	<!-- //web-fonts -->
+	
 </head>
 <body>
+<div class="inner-page-banner" id="home"> 	   
+	<!--Header-->
+	<header>
+		<div class="container agile-banner_nav">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				
+				<h1><a class="navbar-brand" href="home_manager.php">NITC <span class="display"></span></a></h1>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="home_manager.php">Home <span class="sr-only">(current)</span></a>
+						</li>
+						
+						<li class="nav-item">
+						<a class="nav-link" href="allocate_room.php">Allocate Room</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="message_hostel_manager.php">Messages Received</a>
+					</li>
+					<li class="dropdown nav-item">
+						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">Rooms
+							<b class="caret"></b>
+						</a>
+						<ul class="dropdown-menu agile_short_dropdown">
+							<li>
+								<a href="allocated_rooms.php">Allocated Rooms</a>
+							</li>
+							<li>
+								<a href="empty_rooms.php">Empty Rooms</a>
+							</li>
+							<li>
+								<a href="vacate_rooms.php">Vacate Rooms</a>
+							</li>
+						</ul>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="contact_manager.php">Contact</a>
+					</li>
+					<li class="dropdown nav-item">
+						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown"><?php echo $_SESSION['username']; ?>
+							<b class="caret"></b>
+						</a>
+						<ul class="dropdown-menu agile_short_dropdown">
+							<li>
+								<a href="admin/manager_profile.php">My Profile</a>
+							</li>
+							<li>
+								<a href="includes/logout.inc.php">Logout</a>
+							</li>
+						</ul>
+					</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!--Header-->
+</div>
     
     <table class="content-table">
       <thead>
@@ -55,7 +148,32 @@
 
  </table>
 
-
+ <footer class="py-5">
+	<div class="container py-md-5">
+		<div class="footer-logo mb-5 text-center">
+			<a class="navbar-brand" href="http://www.nitc.ac.in/" target="_blank">NIT <span class="display"> CALICUT</span></a>
+		</div>
+		<div class="footer-grid">
+			<div class="list-footer">
+				<ul class="footer-nav text-center">
+					<li>
+						<a href="home_manager.php">Home</a>
+					</li>
+					<li>
+						<a href="allocate_room.php">Allocate</a>
+					</li>
+					<li>
+						<a href="contact_manager.php">Contact</a>
+					</li>
+					<li>
+						<a href="admin/manager_profile.php">Profile</a>
+					</li>
+				</ul>
+			</div>
+			
+		</div>
+	</div>
+</footer>
 
 
 
